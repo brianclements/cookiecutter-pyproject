369a89eaedc12041a1ce095810e80badfd880ae3
Brian Clements <brian@brianclements.net>
2014-09-09 02:04:17 -0700

    Added nonfunctional hook file.
    TODO item.

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#TODO: delete unused licenses.
+#TODO: get all up in versioneer
+#       https://blog.mozilla.org/warner/2012/01/31/version-string-management-in-python-introducing-python-versioneer/
