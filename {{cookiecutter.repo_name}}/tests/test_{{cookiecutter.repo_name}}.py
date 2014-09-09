e62a0bacb07d7b4e62d10a232f4f2cf8ee1f0175
Brian Clements <brian@brianclements.net>
2014-09-09 02:03:38 -0700

    Change tests from package to modules.

---
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -0,0 +1,17 @@
+'''
+Core tests.
+'''
+
+import unittest
+import unittest.mock
+
+from {{cookiecutter.repo_name}} import core
+
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
